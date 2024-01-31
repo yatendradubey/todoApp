edb357f5a7427aa5ba22ab3f20af6b70b3964fca
Yatendra Dubey <yatendradubey.16@gmail.com>
2024-01-31 21:07:01 +0530

    fixed dependency issue

--- src/components/ToDoList/EditTask/EditTask.js
+++ src/components/ToDoList/EditTask/EditTask.js
@@ -15,7 +15,7 @@ const EditTask = (props) => {
   useEffect(() => {
     setTitle(taskDataList[id].title);
     setDescription(taskDataList[id].description);
-  }, [taskDataList]);
+  }, [taskDataList[id]]);
 
   const handleSave = () => {
     console.log("===> save", title, description);
