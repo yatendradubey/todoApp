854e6cb9308f030f85fdcbe7b1a58c1552f55fe1
Yatendra Dubey <yatendradubey.16@gmail.com>
2024-01-31 20:59:29 +0530

    added edit form

--- src/components/ToDoList/EditTask/EditTask.js
+++ src/components/ToDoList/EditTask/EditTask.js
@@ -1,28 +1,57 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./EditTask.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { TaskContext } from "../../../TaskContext";
 
 const EditTask = (props) => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const taskData = useContext(TaskContext);
-  console.log("===> id", id);
-  console.log("===> taskDataEdit", taskData);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [tasks, setTasks] = useState();
+  const taskDataList = taskData.taskList.taskList;
+
+  useEffect(() => {
+    setTitle(taskDataList[id].title);
+    setDescription(taskDataList[id].description);
+  }, []);
+
+  const handleSave = () => {
+    console.log("===> save", title, description);
+    taskDataList[id] = {
+      title: title,
+      description: description,
+      completed: taskDataList[id].completed ? true : false,
+    };
+    setTasks(taskDataList);
+    console.log(taskData);
+    navigate(-1);
+  };
+
   return (
     <div className="editTaskContainer">
       <h2>Edit Task</h2>
       <div className="formContainer">
         <div className="textFieldStyle">
           <label>Title</label>&nbsp;&nbsp;
-          <input type="text"></input>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
         </div>
         <div className="textFieldStyle">
           <label>Description</label>&nbsp;&nbsp;
-          <input type="text"></input>
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></input>
         </div>
       </div>
       <div className="saveButton">
-        <button>Save</button>
+        <button onClick={() => handleSave()}>Save</button>
       </div>
     </div>
   );
